f6b4b22140caa6c005800733b572357fec9ac11e
Bo-wei Chen <rayologist1002@gmail.com>
2021-08-30 16:52:06 +0800

    update api keys

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: "2693b3bd38934314a474d2494fab809b",
+    apiKey: process.env.API_CLARIFAI,
    });
 
 const handleApiCall = (req, res) => {
