d9318ad5366d4710ae3a02a2282d2be1d5e02828
Bo-wei Chen <rayologist1002@gmail.com>
2021-08-30 15:44:21 +0800

    modify port

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db))
 app.put('/image', (req, res) => image.handleImage(req, res, db))
 app.post("/imageUrl", (req, res) => image.handleApiCall(req, res))
 
-const port = 3001;
+const PORT = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
 });
 
 
