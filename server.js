b43d12976bf47292c3281dbf7bae3b0014d48f32
Bo-wei Chen <rayologist1002@gmail.com>
2021-11-29 21:32:43 +0800

    Format using Prettier

--- server.js
+++ server.js
@@ -1,48 +1,47 @@
-const express = require('express');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+const { config } = require("dotenv");
+config();
 
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl:true,
-    }
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
 });
-
 
 const app = express();
 
-app.use(cors())
-app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-
 app.get("/", (req, res) => {
-    res.json("success")
-})
+  res.json({ status: "success" });
+});
 
-app.post("/signin", (req, res) => signin.handleSignIn(req, res, db, bcrypt))
-app.post("/register", (req, res) => register.handleRegister(req, res, db, bcrypt))
-app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db))
-app.put('/image', (req, res) => image.handleImage(req, res, db))
-app.post("/imageUrl", (req, res) => image.handleApiCall(req, res))
+app.post("/signin", (req, res) => signin.handleSignIn(req, res, db, bcrypt));
+app.post("/register", (req, res) =>
+  register.handleRegister(req, res, db, bcrypt)
+);
+app.get("/profile/:id", (req, res) => profile.handleProfile(req, res, db));
+app.put("/image", (req, res) => image.handleImage(req, res, db));
+app.post("/imageUrl", (req, res) => image.handleApiCall(req, res));
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`app is running on port ${PORT}`);
+  console.log(`app is running on port ${PORT}`);
 });
-
 
 /*
 
